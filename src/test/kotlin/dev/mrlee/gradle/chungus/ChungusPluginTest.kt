040f22c0831049ef758d9ed9daa0598f921d67ab
Lee Machin <lee.machin@babylonhealth.com>
2020-04-13 16:39:13 +0100

    Fix up pointless unit tests for now

--- src/test/kotlin/dev/mrlee/gradle/chungus/ChungusPluginTest.kt
+++ src/test/kotlin/dev/mrlee/gradle/chungus/ChungusPluginTest.kt
@@ -1,12 +1,8 @@
 package dev.mrlee.gradle.chungus
 
-import io.mockk.every
-import io.mockk.mockk
-import org.gradle.api.plugins.AppliedPlugin
 import org.gradle.testfixtures.ProjectBuilder
 import kotlin.test.Test
 import kotlin.test.assertNotNull
-import kotlin.test.assertNull
 
 class ChungusPluginTest {
     @Test fun `fetchOpenApiSpecs task is registered`() {
